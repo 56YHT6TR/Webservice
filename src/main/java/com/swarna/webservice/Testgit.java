5c7453a4cc400b2ee23cf365e41310f11208258e
ganesh <ganeshluck202@gmail.com>
2022-10-30 22:00:12 -0500

    added new file

---
+++ src/main/java/com/swarna/webservice/Testgit.java
@@ -0,0 +1,12 @@
+package com.swarna.webservice;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("to test git hub");
+
+	}
+
+}
